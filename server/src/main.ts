b3131830f33790a51f4f39f8282f4673cd9dcebc
danny thibaudeau <cobreti_@hotmail.com>
2018-01-01 13:34:25 -0500

    added https support

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,24 @@
 import * as express from "express";
+// import https = require('https');
+import * as https from "https";
+import * as fs from "fs";
 
 console.log(__dirname);
 
+let options = {
+    key: fs.readFileSync('./certificates/localhost/localhost.key'),
+    cert: fs.readFileSync('./certificates/localhost/localhost.cert'),
+    requestCert: false,
+    rejectUnauthorized: false
+};
+
 let app = express();
+let port = 8001;
+let server = https.createServer(options, app);
 
 app.use( express.static('../web') );
 
 app.listen(8000);
+server.listen(port, function() {
+
+});
